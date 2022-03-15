039286fe789b506ac3783770f45e0a0f2b9a1aaf
Harsh <hjds760781@gmail.com>
2022-03-15 21:58:41 +0530

    initial commit

---
+++ calci/index.js
@@ -0,0 +1,10 @@
+prompt("Enter the first person name : ");
+prompt("Enter the second person name : ");
+
+var lovescore = Math.random();
+lovescore = lovescore * 100 + 1;
+lovescore = Math.floor(lovescore);
+if (lovescore > 80)
+    alert("Your lovescore is " + " " + lovescore + " You are a good couple")
+else
+    alert("Your lovescore is " + " " + lovescore)
